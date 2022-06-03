45b13209c7bc865e02e92410e9403c91a04e26ff
Sabber Hossain <sabberhossain@Sabbers-MacBook-Air.local>
2022-06-03 21:36:21 +0600

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'sabberhossain/e-coder',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sabberhossain/e-coder' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
